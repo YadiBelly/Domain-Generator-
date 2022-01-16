1ab699c561b0fca4057b69edf9baa7055934dc9b
YadiBelly <Yadisabell96@gmail.com>
2022-01-16 16:02:17 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  var pronouns = ["the", "our", "my"];
+  var adjs = ["great", "big"];
+  var nouns = ["jogger", "racoon"];
+
+  for (let pronoun of pronouns) {
+    for (let adj of adjs) {
+      for (let noun of nouns) {
+        document.querySelector("#page").innerHTML += `<li>${pronoun +
+          adj +
+          noun}.com</li>`;
+      }
+    }
+  }
+
   console.log("Hello Rigo from the console!");
 };
